9f73b95f829adaa306aee1699eb3051d6a281198
Alan Tamez <alantamez28@hotmail.com>
2022-03-06 20:53:22 -0600

    Se agrego pagina de not-found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ProjectsComponent} from './components/pages/projects/projects.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component'
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'proyectos', component: ProjectsComponent}
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '', component: HomeComponent },
+  { path: 'proyectos', component: ProjectsComponent },
+  { path: '**', redirectTo: 'not-found', pathMatch: 'full' },
 ];
 
 @NgModule({
