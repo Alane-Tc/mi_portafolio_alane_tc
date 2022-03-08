d23350d02cec7831afb2a9d41919e412b3993009
Alan Tamez <alantamez28@hotmail.com>
2022-03-08 08:55:26 -0600

    Modoficaciones

--- src/app/components/js/app.js
+++ src/app/components/js/app.js
@@ -2,6 +2,7 @@ const scroll = () => {
     let _button = document.querySelector("#btn-go-up");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         _button.style.display = "block";
+        _button.classList.add('animate__animated', 'animate__backInUp')
     } else {
         _button.style.display = "none";
     }
