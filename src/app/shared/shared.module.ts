dcc9529c538b1eb1b969551c2c5bbbe6da25a64a
Alan Tamez <alantamez28@hotmail.com>
2023-03-14 21:23:47 -0600

    Updates

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from "@angular/core";
 import { HeaderFooterModule } from "../components/header-footer/header-footer.module";
 import { CardComponent } from "./components/card/card.component";
+import { LoadingComponent } from "./components/loading/loading.component";
 
 @NgModule({
     declarations: [
         CardComponent,
+        LoadingComponent
     ],
     exports: [
         CardComponent,
+        LoadingComponent
     ],
     imports: [
         HeaderFooterModule,
