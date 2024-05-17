1b5358df971a11617ddec41573b171677fbd2b29
Alan Tamez <alantamez28@hotmail.com>
2024-05-16 19:02:53 -0600

    Updates

--- src/app/components/pages/about-me/about-me.component.ts
+++ src/app/components/pages/about-me/about-me.component.ts
@@ -12,4 +12,7 @@ export class AboutMeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  openPDF() {
+    window.open('https://cdn-files-occ.s3.amazonaws.com/documents/yc/YCJfV7Ia_Kh2E84D8ku3zQ2/resumefile/CV-ALAN-EDUARDO-TAMEZ-CABELLO_ACT.2023.pdf?AWSAccessKeyId=AKIAVBUMCCY2NGVEAT5Y&Expires=1715908335&Signature=oRnETr69pgzH68rP%2Fag4YwYo00A%3D', '_blank');
+  }
 }
